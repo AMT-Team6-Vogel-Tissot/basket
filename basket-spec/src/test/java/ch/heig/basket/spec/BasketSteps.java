2c49bf92082590943c575e1a9f2e61ea7f371bda
maelle <maelle.vogel@heig-vd.ch>
2023-01-26 18:09:17 +0100

    ajout des deux scénarios de test

--- basket-spec/src/test/java/ch/heig/basket/spec/BasketSteps.java
+++ basket-spec/src/test/java/ch/heig/basket/spec/BasketSteps.java
@@ -8,7 +8,6 @@ import org.openapitools.client.ApiResponse;
 import org.openapitools.client.api.BasketEndPointApi;
 import org.openapitools.client.model.Player;
 import org.openapitools.client.model.Team;
-import org.openapitools.client.model.Trophy;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -17,18 +16,15 @@ public class BasketSteps {
 
     private final BasketEndPointApi api = new BasketEndPointApi();
     private Player player;
+
+    private int playerId;
     private int statusCode;
+    private int teamId;
+
+    private String teamName;
 
 
-    @When("I POST it to the \\/quotes endpoint")
-    public void i_POST_it_to_the_quotes_endpoint() throws Throwable {
-        //    try {
-        //     ApiResponse response = api.addQuoteWithHttpInfo(quote);
-        //    statusCode = response.getStatusCode();
-        //  } catch (ApiException e) {
-        //  statusCode = e.getCode();
-        //  }
-    }
+
 
     @Given("I have an player payload")
     public void iHaveAnPlayerPayload() {
@@ -36,12 +32,13 @@ public class BasketSteps {
         player.setId(42);
         player.setName("Olivier");
         player.setSurname("Tissot");
+        player.setFkTeam(2);
     }
 
     @When("I POST it to the \\/players endpoint")
     public void iPOSTItToThePlayersEndpoint() {
         try {
-            ApiResponse response = api.addPlayerWithHttpInfo(player);
+            ApiResponse<Player> response = api.addPlayerWithHttpInfo(player);
             statusCode = response.getStatusCode();
         } catch (ApiException e) {
             statusCode = e.getCode();
@@ -51,5 +48,43 @@ public class BasketSteps {
     @Then("I receive a {int} status code")
     public void i_receive_a_status_code(int arg1) throws Throwable {
         assertEquals(arg1, statusCode);
+    }
+
+    @Given("I have the team id {int}")
+    public void iHaveTheTeamId(int arg0) {
+        teamId = arg0;
+    }
+
+    @When("I GET it to the \\/teams\\/id endpoint")
+    public void iGETItToTheTeamsIdEndpoint() {
+        try {
+            Team response = api.getTeam(teamId);
+            teamName = response.getName();
+        } catch (ApiException e) {
+            statusCode = e.getCode();
+        }
+    }
+
+    @Then("I receive {string}")
+    public void iReceive(String arg0) {
+        assertEquals(arg0, teamName);
+
+    }
+
+    @Given("I have the player id {int} and a playload")
+    public void iHaveThePlayerIdAndAPlayload(int arg0) {
+        playerId = arg0;
+        player = new Player();
+        player.setSurname("Tissot-Daguette");
+    }
+
+    @When("I PATCH it to the \\/players\\/\\{id} endpoint")
+    public void iPATCHItToThePlayersIdEndpoint() {
+        try {
+            ApiResponse<Player> response = api.patchPlayerWithHttpInfo(playerId, player);
+            statusCode = response.getStatusCode();
+        } catch (ApiException e) {
+            statusCode = e.getCode();
+        }
     }
 }
