adf5c9e523a71a3f30d2e81149895d6c4cb1a917
maelle <maelle.vogel@heig-vd.ch>
2023-01-27 18:11:04 +0100

    ajout des endpoints pour les equipes

---
+++ basket-api/src/main/java/ch/heig/basket/api/services/TeamService.java
@@ -0,0 +1,55 @@
+package ch.heig.basket.api.services;
+
+import ch.heig.basket.api.entities.BasketTeam;
+import ch.heig.basket.api.exceptions.TeamNotFoundException;
+import ch.heig.basket.api.repositories.TeamRepository;
+import org.modelmapper.ModelMapper;
+import org.openapitools.model.Team;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class TeamService {
+
+    private final TeamRepository teamRepository;
+
+    private final ModelMapper modelMapper;
+
+    public TeamService(TeamRepository teamRepository) {
+        this.teamRepository = teamRepository;
+
+        modelMapper = new ModelMapper();
+    }
+
+    public List<Team> getTeams(){
+        List<BasketTeam> teamEntities = teamRepository.findAll();
+        List<Team> teams = new ArrayList<>();
+
+        for (BasketTeam teamEntity : teamEntities) {
+            Team t;
+            t = modelMapper.map(teamEntity, Team.class);
+            teams.add(t);
+        }
+
+        return teams;
+    }
+
+    public Team getTeam(int id){
+        BasketTeam teamEntity = teamRepository.findById(id);
+
+        return modelMapper.map(teamEntity, Team.class);
+    }
+
+
+
+    public int addTeam(Team team) {
+        Team newTeam;
+        BasketTeam basketTeam = teamRepository.save(new BasketTeam(team.getId(), team.getName()));
+        newTeam = modelMapper.map(basketTeam, Team.class);
+
+        return newTeam.getId();
+    }
+
+}
